baa10e363548feb4211104d38785060184a431b3
egoldstein13 <esther.goldstein13@gmail.com>
2018-11-14 16:43:30 -0800

    initial commit

--- app/src/main/java/cs147/flexivoice/HomePage.java
+++ app/src/main/java/cs147/flexivoice/HomePage.java
@@ -11,7 +11,7 @@ import android.view.MenuItem;
 
 public class HomePage extends AppCompatActivity {
 
-    @Override
+    @Override 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
